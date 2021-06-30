dfed12b82c0ed9fad27c7fcde2acb8701e602ffe
0xFC963F18DC21 <mfabdullah2002@gmail.com>
2021-06-30 13:00:43 +0100

    Maybe this is unbuffered now?

--- src/main/java/net/nergi/programtimestamper/Main.java
+++ src/main/java/net/nergi/programtimestamper/Main.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.LocalTime;
-import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,9 +15,10 @@ public class Main {
             final var process = builder.start();
             final var outThread = new Thread(
                 () -> {
-                    try (var reader = new Scanner(process.getInputStream())) {
-                        while (reader.hasNextLine()) {
-                            System.out.printf("[%s %s] %s%n", LocalDate.now(), LocalTime.now(), reader.nextLine());
+                    try (var reader = new InputStreamReader(process.getInputStream())) {
+                        String line = null;
+                        while ((line = readLineUnbuffered(reader)) != null) {
+                            System.out.printf("[%s %s] %s%n", LocalDate.now(), LocalTime.now(), line);
                         }
                     } catch (Exception e) {
                         System.err.println("Error when reading from program output:\n" + e);
@@ -49,5 +49,20 @@ public class Main {
         builder.redirectErrorStream(true);
 
         return builder;
+    }
+
+    private static String readLineUnbuffered(InputStreamReader reader) throws IOException {
+        final var sb = new StringBuilder();
+        int cur = 0;
+
+        while ((cur = reader.read()) != '\n') {
+            if (cur == -1) {
+                return null;
+            }
+
+            sb.append((char) cur);
+        }
+
+        return sb.toString();
     }
 }
