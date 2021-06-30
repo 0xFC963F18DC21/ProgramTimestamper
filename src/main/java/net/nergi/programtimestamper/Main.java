b4022298a6a61b37aff4aaaa5719231d838b433c
Fawwaz Abdullah <crashmacompilers@gmail.com>
2021-06-30 12:40:29 +0100

    Replaced BufferedReader with Scanner.

--- src/main/java/net/nergi/programtimestamper/Main.java
+++ src/main/java/net/nergi/programtimestamper/Main.java
@@ -1,10 +1,10 @@
 package net.nergi.programtimestamper;
 
-import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -16,12 +16,11 @@ public class Main {
             final var process = builder.start();
             final var outThread = new Thread(
                 () -> {
-                    try (var reader = new BufferedReader(new InputStreamReader(process.getInputStream()))) {
-                        String line = null;
-                        while ((line = reader.readLine()) != null) {
-                            System.out.printf("[%s %s] %s%n", LocalDate.now(), LocalTime.now(), line);
+                    try (var reader = new Scanner(process.getInputStream())) {
+                        while (reader.hasNextLine()) {
+                            System.out.printf("[%s %s] %s%n", LocalDate.now(), LocalTime.now(), reader.nextLine());
                         }
-                    } catch (IOException e) {
+                    } catch (Exception e) {
                         System.err.println("Error when reading from program output:\n" + e);
                     }
                 }
